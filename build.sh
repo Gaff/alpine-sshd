31fba6e1c6212b9d7796f2378e87a6ff6b12b652
Matthew Shaylor <github@shaylor.co.uk>
2015-12-14 21:58:18 +0000

    Tighter build.

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@ set -ex
 cd /tmp
 apk upgrade
 apk update
+
 apk add openssh rsync
 mkdir -p ~root/.ssh && chmod 700 ~root/.ssh/
+sed -i '/AuthorizedKeysFile/s/^/#/' /etc/ssh/sshd_config
 echo -e "Port 22\n" >> /etc/ssh/sshd_config
+#Add an extra AuthorizedKeysFile so that people can hard-bake their own keys if they wish.
 echo -e "AuthorizedKeysFile .ssh/authorized_keys /authorized_keys\n" >> /etc/ssh/sshd_config
-cp -a /etc/ssh /etc/ssh.cache
+
 #apk del <applications that were used only for building, like gcc, make etc.>
 rm -rf /tmp/*
 rm -rf /var/cache/apk/*
--- service.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/with-contenv ash
-exec /usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
